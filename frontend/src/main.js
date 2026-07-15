98cda842121deb4b119dab921fc41024c0c212f8
chaeone02 <sandy12150831@gmail.com>
2026-07-15 09:59:34 +0900

    feat: 장소 상세 라우팅과 지도 클러스터 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import 'leaflet/dist/leaflet.css'
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 import './style.css'
 import App from './App.vue'
+import router from './router/index.js'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
